f34cd526a29385c304b68fa4ba0588149426963c
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 00:22:23 +0530

    Created Arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,4 @@
+var arrayOfNumber = [1, 2, 3];
+arrayOfNumber = [3, 4, 5];
+// arrayOfNumber=['one','two'] ❌
+console.log(arrayOfNumber);
---
+++ Arrays/arrays.ts
@@ -0,0 +1,6 @@
+var arrayOfNumber:number[]=[1,2,3]
+arrayOfNumber=[3,4,5]
+
+// arrayOfNumber=['one','two'] ❌
+
+console.log(arrayOfNumber)
