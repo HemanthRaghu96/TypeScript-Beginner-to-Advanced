ad62d5279afa59e0f90c5902d11034e2bda73b61
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 00:18:10 +0530

    Created duckTyping

---
+++ Duck-typing/duckTyping.js
@@ -0,0 +1,6 @@
+var obj = { name: "Hemanth", age: 26 };
+//All key value paie is requires once it is defined:
+// obj={age:20} ❌
+// obj={name:"Kiran"} ❌
+obj = { age: 27, name: "Kiran" };
+console.log(obj);
