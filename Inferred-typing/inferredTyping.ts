a097005184d6cdd8b3754516b4dacda081cb01d6
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 00:09:47 +0530

    Created inferredTyping

---
+++ Inferred-typing/inferredTyping.ts
@@ -0,0 +1,11 @@
+var firstName="Hemanth"
+var age=26
+var isMarried=false
+
+//No need to assign type it will auto assign to the given value
+
+// firstName=age ❌
+// age=isMarried ❌
+// isMarried=firstName ❌
+
+console.log(firstName,age,isMarried)
