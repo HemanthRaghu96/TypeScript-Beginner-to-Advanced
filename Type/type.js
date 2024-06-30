908f26fdd54772232ace15df789df96b37ceb9f3
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 00:04:04 +0530

    Created Type

---
+++ Type/type.js
@@ -0,0 +1,8 @@
+var firstName = "Hemanth";
+var age = 26;
+var isMarried = false;
+//Cannot Assign diff datatype to assigned variable type
+// firstName=age ❌
+// age=isMarried ❌
+// isMarried=firstName ❌
+console.log(firstName, age, isMarried);
---
+++ Type/type.ts
@@ -0,0 +1,11 @@
+var firstName:string="Hemanth"
+var age:number=26
+var isMarried:boolean=false
+
+//Cannot Assign diff datatype to assigned variable type
+
+// firstName=age ❌
+// age=isMarried ❌
+// isMarried=firstName ❌
+
+console.log(firstName,age,isMarried)
