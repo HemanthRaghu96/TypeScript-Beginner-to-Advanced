6e25cc00777466e418a3bcbebdd6ff05ec48a2c0
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 11:15:56 +0530

    Created defaultParameters

---
+++ Default-parameters/defaultParameters.js
@@ -0,0 +1,6 @@
+var add = function (a, b, c) {
+    if (c === void 0) { c = 3; }
+    return a + b + c;
+};
+console.log(add(1, 2, 3));
+console.log(add(1, 2));
---
+++ Default-parameters/defaultParameters.ts
@@ -0,0 +1,5 @@
+var add=(a:number,b:number,c:number=3)=>{
+    return a+b+c
+}
+console.log(add(1,2,3))
+console.log(add(1,2))
