1173657a746b9fdbd7e4afcc29c296ebb51d99d2
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-01 11:11:28 +0530

    Created optionalParameters

---
+++ Optional-parameters/optionalParameters.js
@@ -0,0 +1,5 @@
+var add = function (a, b, c) {
+    return a + b + c;
+};
+console.log(add(1, 2, 3));
+console.log(add(1, 2));
---
+++ Optional-parameters/optionalParameters.ts
@@ -0,0 +1,5 @@
+var add=(a:number,b:number,c?:number | undefined)=>{
+    return a+b+c
+}
+console.log(add(1,2,3))
+console.log(add(1,2))
